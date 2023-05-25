db572e6d11b671b771e845c5597ac7e105949534
Chris <104800006+renners7777@users.noreply.github.com>
2023-05-25 21:56:16 +0100

    First commit

---
+++ 2-globals.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Node Tutorial");
+
+// Globals - NO WINDOW!!!
+
+//__dirname     - path to current directory
+//__filename    - file name
+// require      - function to use modules (CommonJS)
+// module       - info about current module (file) - OS, PATH, FS, HTTP
+// process      - info about env where the program is being executed
+
+console.log(__dirname)
+// setInterval(() =>{
+//     console.log("Hello World!")
+// }, 2000)
